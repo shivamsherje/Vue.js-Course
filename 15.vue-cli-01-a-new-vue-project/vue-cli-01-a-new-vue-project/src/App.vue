c0b1d99f4d74d9a6e0167822c807d538054483eb
shivamsherje <sherjes49@gmail.com>
2023-07-04 09:39:50 +0530

    UI improvements

---
+++ 15.vue-cli-01-a-new-vue-project/vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <section>
+    <header><h1>my friends</h1></header>
+    <ui>
+      <friend-contact
+        v-for="friend in friends"
+        :key="friend.id"
+        :name="friend.name"
+        :id="friend.id"
+        :phonenumber="friend.phone"
+        :emailadres="friend.email"
+        :isFavourtie="friend.isFavourtie"
+        @toggle-favorite="onToggleFavorite"
+      ></friend-contact>
+    </ui>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      friends: [
+        {
+          id: "shivame",
+          name: "shivam sherje",
+          phone: "375893429",
+          email: "sherje@gmail.com",
+          isFavourtie: "true",
+        },
+        {
+          id: "juli",
+          name: "juli sherje",
+          phone: "375893429",
+          email: "juli@gmail.com",
+          isFavourtie: "false",
+        },
+      ],
+    };
+  },
+  methods: {
+    onToggleFavorite(friendID) {
+      const identifiedfried = this.friends.find(
+        (friend) => friend.id == friendID
+      );
+      identifiedfried.isFavourtie = !identifiedfried.isFavourtie;
+    },
+  },
+};
+</script>
---
+++ 15.vue-cli-01-a-new-vue-project/vue-cli-01-a-new-vue-project/src/component/FriendContact.vue
@@ -0,0 +1,124 @@
+<template>
+  <section>
+    <ui>
+      <li>
+        <h2>{{ name }}{{ isFavourtie ? "(favourite)" : "" }}</h2>
+        <button @click="toggleFavourite">toggle favorate</button>
+        <button @click="toggleDetails">
+          {{ detailsArevisible ? "hide" : "show" }} details
+        </button>
+        <ui v-if="detailsArevisible">
+          <li><strong> phone:</strong>{{ phonenumber }}</li>
+          <li><strong> Email:</strong>{{ emailadres }}</li>
+        </ui>
+      </li>
+    </ui>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "FriendContact",
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    phonenumber: {
+      type: String,
+      required: true,
+    },
+    emailadres: {
+      type: String,
+      required: true,
+    },
+    isFavourtie: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      detailsArevisible: false,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsArevisible = !this.detailsArevisible;
+    },
+    toggleFavourite() {
+      this.$emit("toggle-favorite", this.id);
+    },
+  },
+};
+</script>
+<style scoped>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Jost", sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+header {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: #58004d;
+  color: white;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+#app li {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 1rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app h2 {
+  font-size: 2rem;
+  border-bottom: 4px solid #ccc;
+  color: #58004d;
+  margin: 0 0 1rem 0;
+}
+
+#app button {
+  font: inherit;
+  cursor: pointer;
+  border: 1px solid #ff0077;
+  background-color: #ff0077;
+  color: white;
+  padding: 0.05rem 1rem;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+}
+
+#app button:hover,
+#app button:active {
+  background-color: #ec3169;
+  border-color: #ec3169;
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+</style>
